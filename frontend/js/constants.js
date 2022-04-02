ceaec80319d50399a62d01865c496c64fd2e770b
CubeDude69420 <wizardspell788@gmail.com>
2022-04-02 11:20:19 +0530

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x429D437a0f17f3956B97B70CD4c5ef5a65585E97"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
